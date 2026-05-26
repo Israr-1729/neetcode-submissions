cd060457783969eaf3f4fd91f2dcddb8cebe2517
Israr-1729 <idnac1729@gmail.com>
2026-05-26 21:58:07 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int size = prices.size();
+        for(int i = 0; i < size-1; i++)
+        {
+            for(int j = i+1; j<size; j++)
+            {
+                if((prices[j] - prices[i])>profit)
+                profit = prices[j] - prices[i];
+            }
+        }
+        return profit;
+    }
+};
