1b1d477da22a22d7570b0122170dfffca4b8fd32
Israr-1729 <idnac1729@gmail.com>
2026-06-23 07:49:36 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        vector<int> freq(26);
+        for(char c : s)
+        freq[c-'a']++;
+
+        for(char c : t)
+        freq[c-'a']--;
+
+        for(int a : freq)
+        {
+            if(a!=0)
+            return false;
+        }
+        return true;
+    }
+};
